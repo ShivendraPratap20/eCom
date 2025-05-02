6de1a3648c33cb4bf2ab8316587ec2968872ad6c
ShivendraPratap20 <shivendragkp2002@gmail.com>
2025-05-02 16:13:57 +0530

    Modify .env file

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,5 +1,4 @@
-require('dotenv').config();
-console.log(require('dotenv').config());
+require('dotenv').config({});
 const express = require("express");
 const cors = require("cors");
 const app = express();
