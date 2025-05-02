dca2bd26926c5b5298af2810e4c65ff2757c0d9a
ShivendraPratap20 <shivendragkp2002@gmail.com>
2025-05-02 23:05:40 +0530

    URL fix

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,5 @@
 const path = require("path");
-//require('dotenv').config({path: path.join(__dirname, "../src/.env")});
-//console.log(require('dotenv').config({path: path.join(__dirname, "./.env")}));
+require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
--- Backend/src/db/conn.js
+++ Backend/src/db/conn.js
@@ -1,6 +1,6 @@
 const mong = require("mongoose");
 //mong.connect(`mongodb://127.0.0.1:27017/eComDB`);
-mong.connect(`mongodb+srv://shivendragkp2002:cfoyy77JeJNth3Qy@cluster0.b6cs8fs.mongodb.net/eComDB?retryWrites=true&w=majority&appName=Cluster0`)
+mong.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_USERKEY}y@cluster0.b6cs8fs.mongodb.net/eComDB?retryWrites=true&w=majority&appName=Cluster0`)
 .then(()=>{
     console.log(`Database connection established`);
 }).catch((err)=>{
